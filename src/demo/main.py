7716b9429769e8e3f8c99d4455b53bb68a96e9d8
Akvgatr <144269819+Akvgatr@users.noreply.github.com>
2025-09-15 16:40:36 +0530

    Prepare for Render deployment

--- src/demo/main.py
+++ src/demo/main.py
@@ -6,6 +6,5 @@ app = create_app()
 
 if __name__ == "__main__":
     print("ok")
-    app.run(host="0.0.0.0",debug=True)
-
-
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port
+    app.run(host="0.0.0.0", port=port, debug=True)
